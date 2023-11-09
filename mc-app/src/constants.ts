a4479edce6ac6d292e5a79b64011d4dcb6dc6193
realnaliboh <realnaliboh@gmail.com>
2023-11-09 12:31:36 +0100

    fiex entryPointUriPath

--- mc-app/src/constants.ts
+++ mc-app/src/constants.ts
@@ -1,7 +1,7 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-export const entryPointUriPath = 'avalara-configuration';
+export const entryPointUriPath = 'avalara';
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
 
