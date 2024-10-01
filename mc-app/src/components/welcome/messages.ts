57985a99e47adf128441903984090a04af083713
realnaliboh <realnaliboh@gmail.com>
2024-10-01 17:46:45 +0200

    # 37206 - Updated mc-app dependencies to remove errors - Added description to the welcome page

--- mc-app/src/components/welcome/messages.ts
+++ mc-app/src/components/welcome/messages.ts
@@ -5,4 +5,39 @@ export default defineMessages({
     id: 'Welcome.title',
     defaultMessage: 'Avalara Tax calculation',
   },
+  info: {
+    id: 'Welcome.info',
+    defaultMessage:
+      'This is a custom application to configure Avalara Tax calculation.',
+  },
+  documentation: {
+    id: 'Welcome.documentation',
+    defaultMessage: 'For more information, check the {link}.',
+  },
+  documentationLink: {
+    id: 'Welcome.documentationLink',
+    defaultMessage:
+      'https://github.com/mediaopt/avalara-commercetools-connector/blob/main/docs/connector_installation_guide.md',
+  },
+  support: {
+    id: 'Welcome.support',
+    defaultMessage: 'If you need help, please contact {link}.',
+  },
+  supportLink: {
+    id: 'Welcome.supportLink',
+    defaultMessage: 'mailto:support@mediaopt.de',
+  },
+  avalara: {
+    id: 'Welcome.avalara',
+    defaultMessage:
+      'For all questions concerning your Avalara account, please contact your Avalara advisor, ',
+  },
+  avalaraHelp: {
+    id: 'Welcome.avalaraHelp',
+    defaultMessage: 'or consult the Avalara Knowledge Center.',
+  },
+  avalaraHelpLink: {
+    id: 'Welcome.avalaraHelpLink',
+    defaultMessage: 'https://knowledge.avalara.com/',
+  },
 });
--- mc-app/src/components/welcome/welcome.tsx
+++ mc-app/src/components/welcome/welcome.tsx
@@ -24,16 +24,37 @@ const Welcome = () => {
     <Constraints.Horizontal max={16}>
       <Grid
         gridGap="16px"
-        gridAutoColumns="1fr"
-        gridTemplateColumns="1fr 3fr 2fr"
+        gridAutoRows='1fr'
+        gridTemplateRows="1fr 3fr 2fr"
       >
-        <Grid.Item>
-          <Text.Headline as="h1" intlMessage={messages.title} />
+        <Grid.Item alignSelf='stretch'>
+          <Text.Headline as="h1" intlMessage={messages.title}/>
+        </Grid.Item>
+
+        <Grid.Item alignSelf='center'>
+          <Text.Body intlMessage={messages.info}/>
+        </Grid.Item>
+
+        <Grid.Item alignSelf='center'>
+          <Text.Body intlMessage={messages.documentation}/>
+          <Link isExternal={true} to={messages.documentationLink.defaultMessage}>GitHub Documentation</Link>
+        </Grid.Item>
+
+        <Grid.Item alignSelf='center'>
+          <Link isExternal={true} to={messages.supportLink.defaultMessage} intlMessage={messages.support}/>
+        </Grid.Item>
+
+        <Grid.Item alignSelf='center'>
+          <Text.Body intlMessage={messages.avalara}/>
+          <Link isExternal={true} to={messages.avalaraHelpLink.defaultMessage} intlMessage={messages.avalaraHelp}/>
+        </Grid.Item>
+
+        <Grid.Item alignSelf='center'>
           <SecondaryButton
             as={Link}
             isExternal={false}
             to={`${match.url}/settings`}
-            label={'Settings'}
+            label={'Procceed to Connector Settings'}
           />
         </Grid.Item>
       </Grid>
